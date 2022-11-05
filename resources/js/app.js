4baa9b59a51b55fa0acb5c49592be2bb669d1067
HananMohammed <hananmohammed2468@gmail.com>
2022-11-05 22:34:21 +0200

    add laravel breeze and configure vue js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { createApp } from 'vue/dist/vue.esm-bundler';
+import PostsIndex from './components/Posts/Index.vue'
+
+const app = createApp({})
+app.component('posts-index', PostsIndex)
+app.mount("#app")
---
+++ resources/js/components/Posts/Index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        comming soon
+    </div>
+</template>
+
+<script>
+export default {
+    name:"PostsIndex"
+}
+</script>
+
+<style scoped>
+
+</style>
