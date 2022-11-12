5d12b2f14c7d5a826ea355561a8caf77cd856490
HananMohammed <hananmohammed2468@gmail.com>
2022-11-12 17:40:35 +0200

    Ordering Data by Clicking Column Titles

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -10,11 +10,20 @@ class PostController extends Controller
 {
     public function index()
     {
+        $orderColumn = request('order_column', 'created_at');
+        if(!in_array($orderColumn, ['id', 'title', 'created_at'])){
+            $orderColumn = 'created_at';
+        }
+        $orderDirection = request('order_direction', 'desc');
+        if(!in_array($orderDirection, ['asc', 'desc'])){
+            $orderDirection = 'desc';
+        }
         $posts = Post::with('category')
                     ->when(request('category'), function ($q){
                         $q->where('category_id', request('category'));
                     })
-                    ->paginate(1);
+                    ->orderBy($orderColumn, $orderDirection)
+                    ->paginate(2);
         return PostResource::collection($posts);
     }
 }
--- resources/js/composables/posts.js
+++ resources/js/composables/posts.js
@@ -2,9 +2,16 @@ import { ref } from 'vue'
 
 export default function usePosts(){
     const posts = ref({});
-    const getPosts = async (page = 1, category='')=>{
-        await axios.get(`/api/posts?page=${page}&category=${category}`)
-            .then( ({data}) => posts.value = data)
+    const getPosts = async (page = 1,
+                            category = '',
+                            order_column = 'created_at',
+                            order_direction = 'desc'
+    ) => {
+        await axios.get(`/api/posts?page=${page}
+                            &category=${category}
+                            &order_column=${order_column}
+                            &order_direction=${order_direction}`)
+            .then(({data}) => posts.value = data)
             .catch(error => console.log(error))
     }
     return {posts, getPosts}
